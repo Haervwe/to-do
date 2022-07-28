6970e56df98e9be8cc434ada9184d9f662c5caad
Haervwe <juan-jose-007@hotmail.com>
2022-07-28 20:13:58 -0300

    add functios to order the list by priority, remove, and mark complete chores

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ ChoreLogicBundle.addChore("test","0sec","1");
 ChoreLogicBundle.addChore("test","0sec","1");
 
 ChoreLogicBundle.removeChore(3);
+ChoreLogicBundle.changeStatus(5);
 ChoreLogicBundle.removeChore(10);
 console.log(ChoreLogicBundle.choresArray);
 
--- src/logic.js
+++ src/logic.js
@@ -14,30 +14,62 @@ const ChoreLogicBundle = (()=>{
 
     const ToDoFactory = (action, esTime, priority)=>{
         id++;
-        let now = new Date();
-        let status = "incomplete"
-        date.format(now, 'YYYY/MM/DD HH:mm:ss');
-        const pattern = date.compile('ddd, MMM DD YYYY');
-        console.log(action, esTime, priority);
+        let creation = new Date();
+        var status = "incomplete"
+        //const pattern = date.compile('ddd, MMM DD YYYY HH:mm');
+        function changeStatusInternal (){
+            let now = new Date();//date.format(new Date(),pattern); 
+            console.log(now);
+            console.log(this.creation);
+            let time = date.subtract(now, this.creation).toMilliseconds();           
+            this.status = `complete in ${time} minutes`;
+            this.priority = 0;
+        }
         return {
             id: id,
             action: action,
             esTime: esTime,
             priority: priority,
-            creation: date.format(now, pattern),
-            status: status
+            creation: creation,
+            status: status,
+            changeStatusInternal,
         }
     };
 
     function addChore (action,esTime,priority){
         let chore = ToDoFactory(action,esTime,priority);
         choresArray.push(chore);
+        orderByPriority()
+    }
+
+    function orderByPriority(){
+        choresArray = choresArray.sort((a,b)=>{
+            if (a.priority < b.priority) {
+                return 1;
+            }
+            if (a.priority > b.priority){
+                return -1;
+            }
+            return 0;
+        });
+    }
+
+    function changeStatus (id){
+        for(let i = 0;i<choresArray.length;i++){
+            if (id == choresArray[i].id){
+                choresArray[i].changeStatusInternal();
+                orderByPriority()
+                return;
+            }
+        }
     }
 
     function removeChore (id){
         for (let i = 0; i<choresArray.length;i++){
             if (id == choresArray[i].id){
                 choresArray.splice(i,1);
+                orderByPriority()
+                return;
             }
         }
     }
@@ -46,6 +78,7 @@ const ChoreLogicBundle = (()=>{
         choresArray,
         addChore,
         removeChore,
+        changeStatus,
     }
 })();
 
